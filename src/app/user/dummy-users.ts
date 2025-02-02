a48dc4a7815e27e36df52205bf4944c1ccd8288c
mohsen_sh5884 <mohsen_sh5884@yahoo.com>
2025-02-01 22:00:00 -0500

    update 02

---
+++ src/app/user/dummy-users.ts
@@ -0,0 +1,10 @@
+export const DUMMY_USERS=[
+  {id:'u1',name:'Mohsen Shahmohammadi',avatar:'user-01.jpg'},
+  {id:'u2',name:'Minoo Nobahari',avatar:'user-02.jpg'},
+  {id:'u3',name:'Ali Ahmadi',avatar:'user-03.jpg'},
+  {id:'u4',name:'Navid Jafari',avatar:'user-04.jpg'},
+  {id:'u5',name:'Ehsan Jahani',avatar:'user-05.jpg'},
+  {id:'u6',name:'Mohamad rezaee',avatar:'user-06.jpg'},
+  {id:'u7',name:'Sahar Moradi',avatar:'user-07.jpg'},
+  {id:'u8',name:'Elahe Alavi',avatar:'user-08.jpg'},
+]
