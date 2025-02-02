2cf229ea64d5d6503745b66abdea73a7e2e04df4
mohsen_sh5884 <mohsen_sh5884@yahoo.com>
2025-02-02 09:49:06 -0500

    update 2

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,4 +1,7 @@
-import { Component } from '@angular/core';
+import { Component,signal } from '@angular/core';
+import { DUMMY_USERS } from './dummy-users';
+
+const randomIndex=Math.floor(Math.random() *DUMMY_USERS.length);
 
 @Component({
   selector: 'app-user',
@@ -7,5 +10,14 @@ import { Component } from '@angular/core';
   styleUrl: './user.component.css'
 })
 export class UserComponent {
+  selectedUser = signal(DUMMY_USERS[randomIndex]);
 
+  get imagePath(){
+    return 'assets/users/'+this.selectedUser().avatar;
+  }
+  onSelected(){
+    const randomIndex=Math.floor(Math.random() *DUMMY_USERS.length);
+    this.selectedUser.set(DUMMY_USERS[randomIndex]);
+    console.log('selected');
+  }
 }
