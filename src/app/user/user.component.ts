fc5eb00035d99749cddf4148bf7c5b88adbe438c
mohsen_sh5884 <mohsen_sh5884@yahoo.com>
2025-02-05 17:14:13 -0500

    input and output decorator

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,7 +1,4 @@
-import { Component, computed, signal } from '@angular/core';
-import { DUMMY_USERS } from './dummy-users';
-
-const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length);
+import { Component, EventEmitter, Input, Output } from '@angular/core';
 
 @Component({
   selector: 'app-user',
@@ -10,15 +7,15 @@ const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length);
   styleUrl: './user.component.css'
 })
 export class UserComponent {
-  selectedUser = signal(DUMMY_USERS[randomIndex]);
-  imagePath = computed(() => 'assets/users/' + this.selectedUser().avatar);
+  @Input({required:true}) id!:string;
+  @Input({required:true}) avatar!:String;
+@Input({required:true}) name!:String;
+@Output() select=new EventEmitter();
 
-  // get imagePath(){
-  //   return 'assets/users/'+this.selectedUser().avatar;
-  // }
   onSelected() {
-    const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length);
-    this.selectedUser.set(DUMMY_USERS[randomIndex]);
-    console.log('selected');
+this.select.emit(this.id);
+  }
+  get imagePath(){
+    return 'assets/users/'+this.avatar
   }
 }
