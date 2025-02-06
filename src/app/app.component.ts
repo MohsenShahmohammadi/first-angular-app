202718223f6f3666855a020f33e4e6f3aef74e93
mohsen_sh5884 <mohsen_sh5884@yahoo.com>
2025-02-06 16:09:03 -0500

    use typescript object

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,17 +3,23 @@ import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './header/header.component';
 import { UserComponent } from "./user/user.component";
 import { DUMMY_USERS } from './user/dummy-users';
+import { TasksComponent } from './tasks/tasks.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, HeaderComponent, UserComponent],
+  imports: [RouterOutlet, HeaderComponent, UserComponent,TasksComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'first-angular-app';
   users = DUMMY_USERS;
+  userSelectedId='u1';
+
+  get selectedUser() {
+    return this.users.find((user) => user.id === this.userSelectedId)!;
+  }
   onSelectedUser(id: string) {
-    console.log('selected user id:' + id);
+    this.userSelectedId=id;
   }
 }
--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -7,15 +7,17 @@ import { Component, EventEmitter, Input, Output } from '@angular/core';
   styleUrl: './user.component.css'
 })
 export class UserComponent {
-  @Input({required:true}) id!:string;
-  @Input({required:true}) avatar!:String;
-@Input({required:true}) name!:String;
+  @Input({required:true}) user!:{
+    id:string;
+    avatar:string;
+    name:string;
+  };
 @Output() select=new EventEmitter();
 
   onSelected() {
-this.select.emit(this.id);
+this.select.emit(this.user.id);
   }
   get imagePath(){
-    return 'assets/users/'+this.avatar
+    return 'assets/users/'+this.user.avatar
   }
 }
